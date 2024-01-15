c42a04eb9a2c69aeb765e691af8de351901069fc
Quang <thvanquang24@gmail.com>
2024-01-15 18:11:40 +0700

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,17 +2,26 @@ import React from "react";
 
 import { bar, account, cart } from "../Assets/icons/Navbar";
 import Logo from "../Assets/logo.png";
+import LogoWhite from "../Assets/logo-white.png";
 
 const Navbar = ({ background }) => {
   return (
-    <div
-      className={`fixed mx-8 mt-4 flex w-[100%] items-center justify-between rounded-full ${background ? `bg-[#2c2b2b]` : `hidden`} p-4`}
-    >
-      <button className="text-white">{bar}</button>
-      <img src={Logo} alt="logo" width="182" height="40" />
-      <div>
-        <button className="mr-2 text-white">{account}</button>
-        <button className="text-white">{cart}</button>
+    <div className="sticky top-[16px] mt-4">
+      <div
+        className={`mx-auto grid w-[97%] grid-cols-[1fr_auto_1fr] items-center justify-between rounded-full bg-[#2c2b2b]  p-4`}
+      >
+        <button className="ml-2 text-white">{bar}</button>
+        <img
+          src={LogoWhite}
+          alt="logo"
+          width="182"
+          height="40"
+          className="scale-75 justify-self-center sm:scale-100"
+        />
+        <div className="justify-self-end">
+          <button className="mr-2 text-white sm:mr-4">{account}</button>
+          <button className="mr-2 text-white">{cart}</button>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,36 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import Navbar from "../Components/Navbar";
 
 import Video from "../Assets/video.mp4";
+
+import { arrow } from "../Assets/icons/Home";
+
+import Bean from "../Assets/triptych-bean.png";
+import Spaceman from "../Assets/triptych-spaceman.png";
+import Cup from "../Assets/triptych-cup.png";
+
+const TRIPTYCH = [
+  {
+    title: "FAST, SIMPLE & CLEAN",
+    image: Bean,
+    altText: "triptych-bean",
+    content:
+      "Prepare easily in minutes. No mess, no machines, no burning or bitterness.",
+  },
+  {
+    title: "YOUR WAY. EVERY TIME",
+    image: Spaceman,
+    altText: "triptych-spaceman",
+    content:
+      "Great for hot & iced drinks. Makes coffees, lattes and so much more.",
+  },
+  {
+    title: "GREAT ON THE GO",
+    image: Cup,
+    altText: "triptych-fast-cup",
+    content: "Enjoy at work or on a trip. TSA approved & designed for travel.s",
+  },
+];
 
 // let prevScrollPosition = window.scrollY;
 
@@ -35,7 +64,9 @@ const Home = () => {
   // console.log(backgroundNavbar);
   return (
     <div className="">
-      <Navbar background={backgroundNavbar} />
+      <Navbar />
+
+      {/* Banner */}
       <div className="-z-50 h-full w-full">
         <video
           playsInline={true}
@@ -47,15 +78,33 @@ const Home = () => {
           <source src={Video} type="video/mp4" />
         </video>
       </div>
+      <div>
+        <div>
+          <h1>Earth's Best Coffee Comes from the Freezer</h1>
+          <p>Precision-Brewed Coffee, Flash-Frozen for Unrivaled Flavor</p>
+        </div>
 
-      <div className="h-28 bg-gray-100">hehe</div>
-      <div className="h-28 bg-gray-200">hehe</div>
-      <div className="h-28 bg-gray-300">hehe</div>
-      <div className="h-28 bg-gray-400">hehe</div>
-      <div className="h-28 bg-gray-300">hehe</div>
-      <div className="h-28 bg-gray-400">hehe</div>
-      <div className="h-28 bg-gray-300">hehe</div>
-      <div className="h-28 bg-gray-400">hehe</div>
+        <button className="flex items-center gap-4 rounded-full bg-[#f5d577] px-10 py-2">
+          TRY COMETEER {arrow}
+        </button>
+      </div>
+
+      {/* Triptych */}
+      <div className="flex max-w-[1320px] flex-nowrap  overflow-scroll">
+        {TRIPTYCH.map((section) => (
+          <div className="flex min-w-[90%] max-w-[800px] items-center justify-center gap-8 border-r-[1px] border-black bg-[#f5d577] p-10 last:border-r-0 lg:min-w-min">
+            <img
+              src={section.image}
+              alt={section.altText}
+              className="max-w-[50px]"
+            />
+            <div>
+              <h6>{section.title}</h6>
+              <p>{section.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
